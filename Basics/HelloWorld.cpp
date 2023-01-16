3509a21869f1a06956687496607ffd0dbafafe1b
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2023-01-16 20:32:40 +0530

    1st C++code

---
+++ Basics/HelloWorld.cpp
@@ -0,0 +1,8 @@
+//Print Hello World Using C++.
+
+#include<iostream>
+using namespace std; //std namespace contains built-in classes & declared functions.
+int main()
+{
+    cout<<"Hello World";
+}
