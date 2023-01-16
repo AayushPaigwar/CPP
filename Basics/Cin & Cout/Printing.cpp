1a90281d8bf05b2fc35f3198d281d7d0dfc308b3
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2023-01-16 21:14:00 +0530

    print the name and age of user

---
+++ Basics/Cin & Cout/Printing.cpp
@@ -0,0 +1,17 @@
+//Write a program to print the name and age of user.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    
+    cout<<"Enter your name:";
+    cin>>name;
+
+    cout<<"Enter your age:";
+    cin>>age;
+
+    cout<<"Your name is "<<name<<" & age is "<<age; 
+}
