e7cbf6ddb18d92b45dec4d5d1a51a036537d9840
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2023-01-16 21:24:20 +0530

    find the greatest of two numbers

---
+++ Basics/Cin & Cout/GreatestOfTwo.cpp
@@ -0,0 +1,14 @@
+//Write a program to find the greatest of two numbers.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    if(a>b)
+    cout<<a<<" is the greatest";
+    else
+    cout<<b<<" is the greatest";
+}
