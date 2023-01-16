91c331e849fc57c898d1b747bac70d1aa8d08267
Aayush Paigwar <90213184+Aayush2711@users.noreply.github.com>
2023-01-16 21:18:36 +0530

    addition of two numbers

---
+++ Basics/Cin & Cout/Add.cpp
@@ -0,0 +1,11 @@
+//Write a program to add two numbers.
+
+#include<iostream>
+using namespace std;
+main()
+{
+    int a,b;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    cout<<a+b<<" is the addtion";
+}
